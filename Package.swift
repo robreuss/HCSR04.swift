e49c59cb7560d302be47c4db0216da9cf3f9c17c
Rob Reuss <robreuss@mac.com>
2019-01-25 14:57:23 -0500

    Re-do of Package.swift

--- Package.swift
+++ Package.swift
@@ -1,23 +1,23 @@
 // swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "HCSR04",
-      products: [
+    products: [
         .library(
             name: "HCSR04",
-            targets: ["HCSR04"],),
-        ],  
+            targets: ["HCSR04"]),
+        ],
     dependencies: [
         .package(url: "https://github.com/robreuss/SwiftyGPIO.git", from: "2.0.0"),
-        ],
-    
-   targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        
+    ],
+    targets: [
         .target(
             name: "HCSR04",
             dependencies: ["SwiftyGPIO"],
                 path: "Sources"),
-    ]
+        ]
 )
