ea3ca2e89d6df8ee08e5fb11a6645f0d83135f15
Rob Reuss <robreuss@mac.com>
2019-01-25 14:18:03 -0500

    Reposition path statement

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "HCSR04",
-            path: "Sources",
-            dependencies: ["SwiftyGPIO"]),
+            dependencies: ["SwiftyGPIO"],
+                path: "Sources"),
     ]
 )
