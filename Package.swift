29550b5dca5872d427901c4f987ad85bc14d4918
Rob Reuss <robreuss@mac.com>
2019-01-25 14:03:23 -0500

    Correction

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     
     dependencies: [
         .package(url: "https://github.com/robreuss/SwiftyGPIO.git", from: "2.0.0"),
-        ]
+        ],
     
    targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
