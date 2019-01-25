5a8ee6e2c33032bc1404713a29b3672569adbba2
Rob Reuss <robreuss@mac.com>
2019-01-25 13:51:15 -0500

    Further compliance with package version 4.0

--- Package.swift
+++ Package.swift
@@ -7,4 +7,12 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/robreuss/SwiftyGPIO.git", from: "2.0.0"),
         ]
+    
+   targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "HCSR04",
+            dependencies: ["SwiftyGPIO"]),
+    ]
 )
