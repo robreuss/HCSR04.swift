e1d75ec911035b9c33f4fdb5b812067e61ad4dcc
Rob Reuss <robreuss@mac.com>
2019-01-25 14:43:47 -0500

    Added library syntax

--- Package.swift
+++ Package.swift
@@ -3,7 +3,11 @@ import PackageDescription
 
 let package = Package(
     name: "HCSR04",
-    
+      products: [
+        .library(
+            name: "HCSR04",
+            targets: ["HCSR04"]),
+        ],  
     dependencies: [
         .package(url: "https://github.com/robreuss/SwiftyGPIO.git", from: "2.0.0"),
         ],
