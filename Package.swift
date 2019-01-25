447c781789dabd9fa2f9b3ec4f126aedb84d71a3
Rob Reuss <robreuss@mac.com>
2019-01-25 13:47:36 -0500

    Increase SwiftyGPIO version to 2.0.0

--- Package.swift
+++ Package.swift
@@ -5,6 +5,6 @@ let package = Package(
     name: "HCSR04",
     
     dependencies: [
-        .package(url: "https://github.com/robreuss/SwiftyGPIO.git", from: "1.0.0"),
+        .package(url: "https://github.com/robreuss/SwiftyGPIO.git", from: "2.0.0"),
         ]
 )
